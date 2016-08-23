15f7f91444782ad1d157af67105fa40f57bc091a
farrp2011 <farrp2011@live.com>
2016-08-23 11:19:59 -0700

    Create Main.c

---
+++ Main.c
@@ -0,0 +1,14 @@
+/*
+* Copy Right Paul Farr - Aug 23, 2016
+* I don't believe in intellectual property rights,
+* So do what ever you want, I'm not responsible for anything bad.
+*/
+
+#include<stdio.h>
+
+int main()
+{
+	printf("\nHello World\n");
+	getc(stdin);
+	return 0;
+}
